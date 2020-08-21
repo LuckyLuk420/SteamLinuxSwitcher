e8013f0fc6d440659c2391f2c63bf263dac73ded
LuckyLuk420 <lpurmom@gmail.com>
2020-08-22 01:02:23 +0200

    added api call

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from api import apiKey
+import requests
 
 app = Flask(__name__)
+steamID = '76561197960434622'
+url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apiKey}&steamid={steamID}'
+rGames = requests.get(url)
 
 
-@app.route('/')
+@app.route('/', methods=['POSt', 'GET'])
 def index():
-    return 'Hello World'
+    if request.method == 'POST':
+        games = {}
+        for i in rGames.json()['response']['rGames']:
+            games[rGames.json()['appid']] = 
+    else:
+        return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
